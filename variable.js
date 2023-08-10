0296d83ee62007e9b9779d0870c9bab975317ba7
arpibi <141954637+arpibi@users.noreply.github.com>
2023-08-10 23:41:19 +0530

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,79 @@
+// var a = 20
+
+// console.log (a)
+
+// var greet = 'Hello'
+// console.log(greet)
+
+//--------------------------------------------------------------------------------------------
+
+// let a=2      //int or number
+// let b='3'   //string
+// console.log(a+b)         // (2+'3') --> ('2'+'3') --> 23
+// let c = a+b
+// console.log(typeof(c))
+
+//when we add string and int, the int will get converted to string automatically
+                            //when we add two strings it's called string concatination
+
+ //-----------------------------------------------------------------------------------------------
+ 
+//  let a= 'Arpita'
+//  let b= 'Anup'
+
+//  console.log(a + b)       //this is called string concatination
+//------------------------------------------------------------------------------
+
+//given two int var, make a calculator with addition, substraction, multipliaction, divide and remainder
+ 
+// let a= 25
+
+// let b= 5
+
+// console.log('the addition is', a+b)   
+// console.log('the substraction is', a-b)   
+// console.log('the multiplication is', a*b)   
+// console.log('the division is', a/b)   
+// console.log('the remainder is', a%b)   
+//---------------------------------------------------------------------------------
+/*
+premitive datatype
+number
+string
+Boolean
+undefined
+null
+
+ */
+// let a = 5
+// let b = 'Anup'
+// let c = false
+// let d 
+// let e = null
+
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+// console.log(typeof(d))
+// console.log(typeof(e))
+
+//------------------creating an object-----------------------------------
+
+// let customerName = 'Anup'
+// let customerAge = 24
+// let customerAddress = 'Haldia'
+// let customerStatus = 'commited'
+// let customerGirlfriend = "Arpita"
+
+// let customer = {
+//     name:'Anup', age:24, address: {
+//         vill: "rukeshpur", post: 'baneswarpur', ps: "balagarh", Dist: "hooghly"
+//     }, Status: "commited", Girlfriend:'Arpita'     //key: value pair
+// }
+// console.log(customer)
+// console.log(customer.name)
+// console.log(customer.address.post)
+//  console.log(customer[0])
+//------------------------------------------------------------------------
+
+//
