92c3574f87de55ea74dff31722f34bb11f321dd0
ARPITA BISWAS <141954637+arpibi@users.noreply.github.com>
2023-08-27 01:52:26 +0530

    Add files via upload

---
+++ 1_arrays.js
@@ -0,0 +1,51 @@
+// Arrays provides you an ordered collection of data
+
+//var arr = [12, 'Ferrari', true, 12.4]
+
+// console.log(arr)
+
+// // accessing the elements
+
+// var a = arr[1]
+
+// console.log(arr[3])
+
+// //replacing elements of an array by using index
+// arr[1] = 'Bentley'
+
+// console.log(arr)
+
+// console.log('The length of the array is', arr.length)
+
+//inbuilt js array methods
+
+var arr2 = [12, 14, 15, 57]
+
+//pop method
+
+var c = arr2.pop()
+console.log(c)
+
+console.log(arr2)  //popped array
+
+//push method
+
+arr2.push(100)
+
+console.log(arr2)  //push array
+
+
+//shift method
+
+var d = arr2.shift()
+console.log(d)
+
+console.log(arr2)  //shifted array
+
+
+
+//unshift array
+
+arr2.unshift(102)
+
+console.log(arr2)   //unshift array
---
+++ 2_objects.js
@@ -0,0 +1,43 @@
+// //in Js objects are key value pair
+
+// // var person = {
+// //     firstName : 'Adam',
+// //     secondName : 'Smith',
+// //     age : 25,
+// //     ownsCar : false
+// // }
+
+// // console.log(person)
+
+// // //dot notation
+
+// // console.log(person.age)  //25
+
+// // // bracket notation
+
+// // console.log(person['firstName'])  //Adam
+
+
+// var cap = {
+//     firstName : 'Steve',
+//     secondName : 'Rogers',
+//     age : 102,
+//     friends : ['Bucky Barnes', 'Bruce Banner', 'Tony stark'],
+//     address : {
+//         state : 'New York',
+//         city : {
+//             name : 'Brooklyn',
+//             pincode : 123456
+//         }
+//     }
+// }
+
+// console.log(cap.friends[1]) //Bruce Banner
+
+// console.log(cap.address.city.name)  //Brooklyn
+
+// cap.movies = ['age of ultron', 'civil war', 'first avenger']
+
+// delete cap.age
+
+// console.log(cap)
