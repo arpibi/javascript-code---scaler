143c221b0b5624c2a20dfc8787858e7ef2c08e78
ARPITA BISWAS <141954637+arpibi@users.noreply.github.com>
2023-08-28 00:49:40 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,28 @@
+// A while loop evaluates the condition inside the parenthesis ().
+// If the condition evaluates to true, the code inside the while loop is executed.
+// The condition is executed again.
+// This process continues until the condition is false.
+// When the condition evaluates to false, the loop stops.
+
+// syntax
+
+// we have to print 1 to 10 using wjile loop
+
+// var i=1
+// var n=10
+
+// while(i<=10){
+//     console.log(i)
+//     i++      //increasing the value by 1
+// }
+
+
+// Do while loop
+
+var i=11
+var n=10
+
+do{
+    console.log(i)
+    i++
+} while (i<=10)         //run at least once
